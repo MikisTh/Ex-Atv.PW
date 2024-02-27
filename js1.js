c154ff05b0752f48b6f89b67ba50b27d614dfab7
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-02-27 19:41:03 -0300

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,10 @@
+let idade = 24;
+let salario = 4568.32;
+let nome = 'w2k';
+let estaSol = true;
+
+console.log(typeof idade);
+console.log(typeof salario);
+console.log(typeof nome);
+console.log(typeof estaSol);
+
